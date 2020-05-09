7fe0a3d9b6f1a3fe30d6f53189a1ab2bf8ce80ea
Denis Donadel <denisdonadel@gmail.com>
2020-05-09 11:26:31 +0200

    Fix a tab

--- src/addGrade.php
+++ src/addGrade.php
@@ -1,17 +1,17 @@
 <title>Vulnerable grades</title>
 <div align="center">
-<form action="addGradeCheck.php" method="POST" id="formAddGrade">
-	<label>Exam date (optional)</label><br/>
-	<input name="date" type="date" id="date" /><br/>
-	<label>Username</label><br/>
-	<input name="username" type="text" id="username" /><br/>
-	<label>Subject</label><br/>
-	<input name="subject" type="text" id="subject" /><br/>
-	<label>Grade</label><br/>
-	<input name="grade" type="text" id="grade" /><br/><br/>
-	<label>Passed</label><br/>
-	<input name="passed" type="checkbox" id="passed" /><br/><br/>
-	<input type="submit" name="Add" value="Add grade" /> 
-</form>
-<a href="admin.php"> Go back </a>
+	<form action="addGradeCheck.php" method="POST" id="formAddGrade">
+		<label>Exam date (optional)</label><br/>
+		<input name="date" type="date" id="date" /><br/>
+		<label>Username</label><br/>
+		<input name="username" type="text" id="username" /><br/>
+		<label>Subject</label><br/>
+		<input name="subject" type="text" id="subject" /><br/>
+		<label>Grade</label><br/>
+		<input name="grade" type="text" id="grade" /><br/><br/>
+		<label>Passed</label><br/>
+		<input name="passed" type="checkbox" id="passed" /><br/><br/>
+		<input type="submit" name="Add" value="Add grade" />
+	</form>
+	<a href="admin.php"> Go back </a>
 </div>
